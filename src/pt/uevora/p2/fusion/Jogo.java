d44c8c2a152eae356041e1c3126bedbb9ffc217c
Eduardo Medeiros <theedfame@gmail.com>
2018-06-01 21:48:09 +0100

    EXIT_ON_CLOSE. cleaned imports.

--- src/pt/uevora/p2/fusion/Jogo.java
+++ src/pt/uevora/p2/fusion/Jogo.java
@@ -3,6 +3,8 @@ package pt.uevora.p2.fusion;
 public class Jogo {
 
     public static void main(String[] args) {
-        System.out.println("this is a fucking test");
+        Window win = new Window(4);
+        win.setVisible(true);
+
     }
 }
