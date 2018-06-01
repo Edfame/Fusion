5ecb7b3cf0985a2f3ef1426c77b148ceafb999dd
Eduardo Medeiros <theedfame@gmail.com>
2018-06-01 22:09:39 +0100

    added dois

--- src/pt/uevora/p2/fusion/Test.java
+++
@@ -1,6 +0,0 @@
-package pt.uevora.p2.fusion;
-
-public class Test {
-
-    int slav = 20;
-}
--- src/pt/uevora/p2/fusion/Teste.java
+++ src/pt/uevora/p2/fusion/Teste.java
@@ -2,4 +2,5 @@ package pt.uevora.p2.fusion;
 
 public class Teste {
     int ahs=3;
+    int dois = 20;
 }
